74536bee3afa28f8519d8a9e467a5c8061eb21e4
snake0 <1260865816@qq.com>
2019-08-27 22:50:47 +0800

    format code

--- solution.cc
+++ solution.cc
@@ -323,15 +323,15 @@ public:
   }
   */
 
-  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    if (!l1 || (l2 && l1->val > l2->val))
-      std::swap(l1, l2);
-    if (l1)
-      l1->next = mergeTwoLists(l1->next, l2);
-    return l1;
-  }
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1 || (l2 && l1->val > l2->val))
+            std::swap(l1, l2);
+        if (l1)
+            l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    }
 };
 
 int main() {
-  cout << "hello world";
+    cout << "hello world";
 }
