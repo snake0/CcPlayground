bdc1fa22e71c4894440ac0c633e6e3f35d1b13fd
snake0 <1260865816@qq.com>
2019-09-22 18:42:13 +0800

    pthread_barrier

--- c_learning/pthread_barrier.c
+++ c_learning/pthread_barrier.c
@@ -3,121 +3,44 @@
 //
 
 #include "include_for_c.h"
-
-#define NTHR 8     /*  线程数  */
-#define NUMBER 8000000L  /* 排序数 */
-#define TNUM  (NUMBER/NTHR)  /* 每个线程平均排序数 */
-
-long nums[NUMBER];
-long snums[NUMBER];
+#include "lock.h"
 
 pthread_barrier_t barrier;
 
-/* 这里引用了一个外部函数  来自bsd库的堆排序函数 SOLARIS系统内置函数名为 qsort*/
-#ifdef SOLARIS
-#define heapsort qsort
-#else
-//extern int qsort(void *__base, size_t __nmemb, size_t __size,
-//                 __compar_fn_t __compar) __nonnull ((1, 4));
-#endif   //SOLARIS
+volatile int count = 0;
+spinlock_t spinlock;
 
-/**
- * 比较两个long integers(helper function for heapsort)
- */
-
-int complong(const void *arg1, const void *arg2) {
-  long l1 = *(long *) arg1;
-  long l2 = *(long *) arg2;
-  if (l1 == l2)
-    return 0;
-  else if (l1 < l2)
-    return -1;
-  else
-    return 1;
-}
-
-
-/**
- * worker thread to sort a portion of the set of numbers
- */
-void *thr_fn(void *arg) {
-  long idx = (long) arg;
-  qsort(&nums[idx], TNUM, sizeof(long), complong);
+void *thread(void *arg) {
+  spinlock_lock(&spinlock);
+  ++count;
+  spinlock_unlock(&spinlock);
   pthread_barrier_wait(&barrier);
-
-  /**
-   * 退出或者做其他更多工作.....
-   */
-
-  return ((void *) 0);
-}
-
-/**
- * Merge the results of the individual sorted ranges
- */
-void merge() {
-  long idx[NTHR];
-  long i, minidx, sidx, num;
-  for (i = 0; sidx < NUMBER; i++)
-    idx[i] = i * TNUM;
-  for (sidx = 0; sidx < NUMBER; sidx++) {
-    num = INT64_MAX;
-    for (i = 0; i < NTHR; i++) {
-      if ((idx[i] < (i + 1) * TNUM) && (nums[idx[i]] < num)) {
-        num = nums[idx[i]];
-        minidx = i;
-      }
-    }
-    snums[sidx] = nums[idx[minidx]];
-    idx[minidx]++;
-  }
+  printf("%d ",count);
 
 }
 
-int main(int argc, char **argv) {
-  unsigned long i;
-  struct timeval start, end;
-  long long startusec, endusec;
-  double elapsed;
-  int err;
-  pthread_t tid;
+int main(int argc, char *argv[]) {
+  int thread_num = -1;
 
-  /**
-   * 创建初始数字集进行排序
-   */
-  srandom(1);
-
-  for (i = 0; i < NUMBER; i++)
-    nums[i] = random();
-  /**
-   * 创建八个线程去排序数字
-   */
-
-  gettimeofday(&start, NULL);
-  pthread_barrier_init(&barrier, NULL, NTHR + 1);   /* 初始化屏障 */
-  for (i = 0; i < NTHR; i++) {
-    err = pthread_create(&tid, NULL, thr_fn, (void *) (i * TNUM));
-    if (err != 0)
-      exit(err);
+  if (argc == 2) {
+    thread_num = atoi(argv[1]);
+  } else {
+    printf("usage: ddd [thread_num]\n");
+    return 1;
   }
 
-  pthread_barrier_wait(&barrier);   /* 获取屏障锁 */
-  merge();   /* 合并 */
-  gettimeofday(&end, NULL);
+  spinlock_init(&spinlock);
+  pthread_barrier_init(&barrier, 0, thread_num);
+  pthread_t *pids = (pthread_t *) malloc(sizeof(pthread_t) * thread_num);
+  for (int i = 0; i < thread_num; ++i) {
+    pthread_create(&pids[i], 0, thread, 0);
+  }
+//  pthread_barrier_wait(&barrier);
 
-  /**
-   * 打印排序后当清单
-   */
+  printf("create done\n");
 
-  startusec = start.tv_sec * 1000000 + start.tv_usec;
-  endusec = end.tv_sec * 1000000 + end.tv_usec;
-
-  elapsed = (double) (endusec - startusec) / 1000000.0;
-
-  printf("sort took %.4f seconds\n", elapsed);
-
-  for (i = 0; i < NUMBER; i++)
-    printf("%ld\n", snums[i]);
-  exit(0);
+  for (int i = 0; i < thread_num; ++i) {
+    pthread_join(pids[i], 0);
+  }
 
 }
