f0aad4c8c441e5c8ff541561986df07c678b5275
snake0 <1260865816@qq.com>
2019-10-04 10:43:50 +0800

    events done

--- c_learning/events.c
+++ c_learning/events.c
@@ -5,35 +5,44 @@
 #include "lock.h"
 #include "include_for_c.h"
 
-#define THREADS 10
+#define THREADS 2
 pthread_t threads[THREADS];
+
+pthread_barrier_t barrier;
+
 
 static int events = 5;
 spinlock_t lk = {0};
 
 int get_event(void) {
-  spinlock_lock(&lk);
-  if (events == 0) {
+    spinlock_lock(&lk);
+    if (events == 0) {
+        spinlock_unlock(&lk);
+        return 0;
+    }
+    --events;
     spinlock_unlock(&lk);
-    return 0;
-  }
-  --events;
-  spinlock_unlock(&lk);
-  return 1;
+    return 1;
 }
 
 void *thread(void *arg) {
-  while (get_event())
-    printf("%d, %d\n", (int)pthread_self(),events);
-  return (void *) 0;
+    pthread_barrier_wait(&barrier);
+
+    while (get_event()) {
+        sleep(1);
+        printf("%d, %d\n", (int) pthread_self(), events);
+    }
+    return (void *) 0;
 }
 
 
 int main() {
-  for (int i = 0; i < THREADS; ++i) {
-    pthread_create(&threads[i], NULL, thread, NULL);
-  }
-  for (int i = 0; i < THREADS; ++i) {
-    pthread_join(threads[i], NULL);
-  }
+    pthread_barrier_init(&barrier, 0, THREADS);
+
+    for (int i = 0; i < THREADS; ++i) {
+        pthread_create(&threads[i], NULL, thread, NULL);
+    }
+    for (int i = 0; i < THREADS; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 }
